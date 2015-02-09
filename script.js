fa73ff98bea0e63f233aa2ed5c7b3fe9cc4e0a64
Brian Piejak <Brian.Piejak@gmail.com>
2015-02-08 23:13:29 -0500

    Made generic and added a random date side as well as a landing page

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var randomImages = ["image1.png", "image2.png", "image3.png", "image4.png", "image5.png", "image6.png", "image7.png", "image8.png", "image9.png", "image10.png", ];
+var randomconvos = ["image1.png", "image2.png", "image3.png", "image4.png", "image5.png", "image6.png", "image7.png", "image8.png", "image9.png", "image10.png", ];
 
-function getRandomImage(imgAr){
-  var num = Math.floor(Math.random() * imgAr.length);
-  var img = imgAr[num];
+function getRandomConvo(comAr){
+  var num = Math.floor(Math.random() * comAr.length);
+  var img = comAr[num];
   var imgStr = '<img src="' + img + '" alt = "something went wrong">';
   document.write(imgStr); document.close();
+}
+
+var randomdates = ["date1.png", "date2.png", "date3.png", "date4.png", "date5.png", "date6.png", "date7.png", "date8.png", "date9.png", "date10.png", "date11.png", ];
+
+function getRandomDate(dateAr){
+  var number = Math.floor(Math.random() * dateAr.length);
+  var image = dateAr[number];
+  var imageStr = '<img src="' + image + '" alt = "something went wrong">';
+  document.write(imageStr); document.close();
 }
