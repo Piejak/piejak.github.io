d22443cfac272eccc48e0b39f359068a84c10e0a
Brian Piejak <Brian.Piejak@gmail.com>
2015-03-22 23:14:33 -0400

    Add scoreboard app
    A simple scoreboard app styled with materialize. I plan to add more
    functionality and customization later.

---
+++ js/scoreboard.js
@@ -0,0 +1,12 @@
+var blue = 0;
+var red = 0;
+
+function blueteamscore(){
+  blue++;
+  document.getElementById('blue-score').innerHTML = blue;
+}
+
+function redteamscore(){
+  red++;
+  document.getElementById('red-score').innerHTML = red;
+}
