76b2a61f6da520ff470f4a9b60c46dbd532c3eff
Brian Piejak <Brian.Piejak@gmail.com>
2015-03-26 22:01:48 -0400

    Add homepage listing projects and update scoreboard
    Scoreboard now allows users to change the name of the teams.

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -10,3 +10,11 @@ function redteamscore(){
   red++;
   document.getElementById('red-score').innerHTML = red;
 }
+
+function bluename(){
+  document.getElementById('blue-name').innerHTML = prompt("Enter a new name") + '' + '<button class="write btn-floating btn-large red lighten-1" onclick="bluename()"><i class="mdi-content-create"></i></button>';
+}
+
+function redname(){
+  document.getElementById('red-name').innerHTML = prompt("Enter a new name") + '' + '<button class="write btn-floating btn-large light-blue" onclick="redname()"><i class="mdi-content-create"></i></button>';
+}
